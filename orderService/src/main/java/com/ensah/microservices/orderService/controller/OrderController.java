cb96f265c501c9d3f2e8644f71a09a13151eb470
OUSSAMA EL ALAOUI EL ISMAILI <oussama.elalaouielismali@etu.uae.ac.ma>
2025-02-15 15:26:49 +0100

    finishing orderService

--- orderService/src/main/java/com/ensah/microservices/orderService/controller/OrderController.java
+++ orderService/src/main/java/com/ensah/microservices/orderService/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.ensah.microservices.orderService.controller;
 import com.ensah.microservices.orderService.dto.OrderDTO;
 import com.ensah.microservices.orderService.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     private final OrderService orderService;
 
+    @PostMapping
     public String placeOrder(@RequestBody OrderDTO orderDTO) {
         orderService.placeOrder(orderDTO);
         return "Order Placed";
--- orderService/src/test/java/com/ensah/microservices/orderService/OrderServiceApplicationTests.java
+++ orderService/src/test/java/com/ensah/microservices/orderService/OrderServiceApplicationTests.java
@@ -1,15 +1,54 @@
 package com.ensah.microservices.orderService;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+	@LocalServerPort
+	private Integer port;
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+	static {
+		mySQLContainer.start();
+	}
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String submittedOrderJSON = """
+				{
+				    "orderNumber":"55555",
+				    "skuCode":"555",
+				    "price": 1000,
+				    "quantity": 1
+				}
+				""";
+		String string = RestAssured.given()
+				.contentType(ContentType.JSON)
+				.body(submittedOrderJSON)
+				.when()
+				.post("api/order")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract()
+				.body()
+				.asString();
+		assertThat(string, Matchers.is("Order Placed"));
 	}
 
 }

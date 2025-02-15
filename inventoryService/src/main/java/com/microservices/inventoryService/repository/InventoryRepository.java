d9707dac9db326147b57efa1618189bd7f55da0a
OUSSAMA EL ALAOUI EL ISMAILI <oussama.elalaouielismali@etu.uae.ac.ma>
2025-02-15 16:20:52 +0100

    finishing inventoryService

---
+++ inventoryService/src/main/java/com/microservices/inventoryService/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.microservices.inventoryService.repository;
+
+import com.microservices.inventoryService.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
---
+++ inventoryService/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,4 @@
+INSERT INTO inventory (sku_code, quantity)
+VALUES ('iphone15', 100),
+       ('iphone14', 200),
+       ('iphone13', 300);

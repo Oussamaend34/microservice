47a2275aec3f029d9bcd33d02c1af3a0056cb4bc
OUSSAMA EL ALAOUI EL ISMAILI <oussama.elalaouielismali@etu.uae.ac.ma>
2025-02-16 20:57:31 +0100

    allowing documentation through apiGateway

--- apiGateway/src/main/java/com/ensah/microservices/gateway/config/SecurityFilterChainConfig.java
+++ apiGateway/src/main/java/com/ensah/microservices/gateway/config/SecurityFilterChainConfig.java
@@ -4,17 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityFilterChainConfig {
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**",
+    "swagger-resources/**", "/api-docs/**", "/aggregate/**"};
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        System.out.println("SecurityConfig springSecurityFilterChain");
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
-                .authenticated())
+        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(freeResourceUrls).permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
--- apiGateway/src/main/java/com/ensah/microservices/gateway/routes/Routes.java
+++ apiGateway/src/main/java/com/ensah/microservices/gateway/routes/Routes.java
@@ -4,12 +4,11 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 
 @Configuration
 public class Routes {
@@ -22,15 +21,39 @@ public class Routes {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoutes() {
+        return GatewayRouterFunctions.route("product-service-swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> orderServiceRoutes() {
         return GatewayRouterFunctions.route("order-service")
                 .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
                 .build();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoutes() {
+        return GatewayRouterFunctions.route("order-service-swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoutes() {
         return GatewayRouterFunctions.route("inventory-service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoutes() {
+        return GatewayRouterFunctions.route("inventory-service-swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 }
